bd1875e485ee75591e5baa54f419ffd2b350b683
zaintanoli <66118772+zaintanoli@users.noreply.github.com>
2025-07-27 06:28:09 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,34 @@
+const score = 400
+//console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.8966
+//console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//   ++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(5.4));
+// console.log(Math.min(5, 3, 9, 4));
+// console.log(Math.max(5, 3, 9, 4));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max -min + 1)));
